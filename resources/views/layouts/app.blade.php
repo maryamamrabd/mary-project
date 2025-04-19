ff1763f2f9e1c1137b6f28be7ffd2819a7d59542
Agmir Hassane <hagmir6@gmail.com>
2025-04-19 17:12:41 +0100

    Add Product page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                 {{ auth()->user()->full_name }}
             </button>
             @else
-                <a href="{{ route(" login") }}"
+                <a href="{{ route("login") }}"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">
                     Rejoignez-nous
                 </a>

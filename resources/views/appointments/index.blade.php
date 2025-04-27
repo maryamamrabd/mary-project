429a8e44cd2e88bad282269c467fd4d96b1905c6
Agmir Hassane <hagmir6@gmail.com>
2025-04-27 14:53:23 +0100

    last update

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -10,15 +10,10 @@
     </div>
     @endif
 
-    @if ($appointments->isEmpty())
-    <div class="text-center text-gray-500">
-        Vous n'avez pas encore de rendez-vous.
-    </div>
-    @else
     <div class="mb-5 flex">
         <a href="{{ route("appointments.create") }}" class="p-2 rounded-lg bg-blue-500 text-white flex gap-3">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M12 5l0 14" />
@@ -27,6 +22,13 @@
             <span>Nouvelles Rendez-vous</span>
         </a>
     </div>
+
+    @if ($appointments->isEmpty())
+    <div class="text-center text-gray-500">
+        Vous n'avez pas encore de rendez-vous.
+    </div>
+    @else
+
     <div class="overflow-x-auto">
         <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
             <thead class="bg-blue-500 text-white">

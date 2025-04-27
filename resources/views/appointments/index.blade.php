39229a521d887eb682e5e66a56d851005841b9ce
Agmir Hassane <hagmir6@gmail.com>
2025-04-27 14:50:42 +0100

    last update

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -15,6 +15,18 @@
         Vous n'avez pas encore de rendez-vous.
     </div>
     @else
+    <div class="mb-5 flex">
+        <a href="{{ route("appointments.create") }}" class="p-2 rounded-lg bg-blue-500 text-white flex gap-3">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 5l0 14" />
+                <path d="M5 12l14 0" />
+            </svg>
+            <span>Nouvelles Rendez-vous</span>
+        </a>
+    </div>
     <div class="overflow-x-auto">
         <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
             <thead class="bg-blue-500 text-white">
